dad186c732d3a28378406dd1ba68c99f2cdd09d0
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2020-02-26 21:26:51 -0500

    [Add] Added restricted middlewear to users router. working.

--- router/router.js
+++ router/router.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const authRouter = require('../auth/auth-router');
 const userRouter =  require('../users/user-router');
+const restricted  = require('../middlewear/restricted-middlewear');
 const router =  express.Router();
 
 
 
 router.use('/auth', authRouter);
-router.use('/users', userRouter);
+router.use('/users', restricted, userRouter);
 
 router.get('/', (req, res) => {
     res.status(200).json({message: 'api end point working'});
